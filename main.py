666a2463680960e68885959d60d550026912a05e
Ricardo Oedietram <ricardo@odracir.nl>
2025-11-17 19:29:13 +0100

    lijst met uitbreidingen voor de volgende release

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #oefening 1: dobbelstenen, gooi 3 dobbelstenen en hoogste wint
 
+#todo
+#1e uitbreiding:
+#uitbreiding maken: 3x gelijke stenen = +5 punten extra
+
+#todo
+#2e uitbreiding:
+# 1 gooien is een strafpunt = aantal 1-en minus totaal
+
+
 import sys
 #todo uitbreidingen
 #todo verzin inzet  geld, max speelronde
